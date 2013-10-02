3821ea4d04904691695a601f83f587dc6e460f92
David Terei <davidterei@gmail.com>
2013-10-01 18:01:49 -0700

    Add utils for space processing.

---
+++ utils.c
@@ -0,0 +1,32 @@
+// Random utility functions needed for memcahce.
+
+#include "utils.h"
+
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Avoid warnings on solaris, where isspace() is an index into an array, and
+// gcc uses signed chars.
+#define xisspace(c) isspace((unsigned char)c)
+
+bool safe_strtol(const char *str, int32_t *out) {
+    assert(out != NULL);
+    errno = 0;
+    *out = 0;
+    char *endptr;
+    long l = strtol(str, &endptr, 10);
+    if ((errno == ERANGE) || (str == endptr)) {
+        return false;
+    }
+
+    if (xisspace(*endptr) || (*endptr == '\0' && endptr != str)) {
+        *out = l;
+        return true;
+    }
+    return false;
+}
+
---
+++ utils.h
@@ -0,0 +1,11 @@
+// Random utility functions needed for memcahce.
+#ifndef _UTILS_H
+#define _UTILS_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+bool safe_strtol(const char *str, int32_t *out);
+
+#endif
+
